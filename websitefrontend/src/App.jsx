5dba7b9c6142625f373c86957142c9a541b5d8b9
SurajGupta7224 <surajgupta3118@gmail.com>
2026-07-08 14:32:03 +0530

    update

--- websitefrontend/src/App.jsx
+++ websitefrontend/src/App.jsx
@@ -1,17 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </>
+    <div>
+      Hello World
+    </div>
   );
 }
 
